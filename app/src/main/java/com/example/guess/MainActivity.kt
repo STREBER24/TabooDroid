af0847a2720aa74e51bf66102c89e513d07dbaa4
STREBER24 <largot@gmx.de>
2023-02-28 23:14:01 +0100

    enable view binding and implement taskFile loading

--- app/src/main/java/com/example/guess/MainActivity.kt
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.guess.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val taskFile = getTaskFile()
+        Toast.makeText(this, "Loaded file with ${taskFile.size.toString()} tasks.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getTaskFile(): List<GuessTask> {
+        val allFiles = assets.list("taskFiles")
+        if (allFiles.isNullOrEmpty()) {
+            return emptyList()
+        }
+        val reader = assets.open("taskFiles/${allFiles[0]}").bufferedReader()
+        return reader.lineSequence()
+            .filter { it.isNotBlank() }
+            .map {
+                val line = it.split(',', ignoreCase = false)
+                GuessTask(line[0], line.slice(1..5))
+            }.toList()
     }
 }
+
+class GuessTask(val guessWord: String, val blockedWords: List<String>)
