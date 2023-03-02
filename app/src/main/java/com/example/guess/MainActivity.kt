759b8339d6fc14099d487f3fb40deb166d685236
STREBER24 <largot@gmx.de>
2023-03-02 23:31:36 +0100

    create state logic

--- app/src/main/java/com/example/guess/MainActivity.kt
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Vibrator
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.guess.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -24,25 +27,56 @@ class MainActivity : AppCompatActivity() {
 
         @Suppress("DEPRECATION")
         val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        val timer = getTimer(vibrator)
+        val timer = object : StatefulTimer() {
+            override fun onTick(secondsUntilFinished: Int) {
+                binding.timerText.text = secondsUntilFinished.toString()
+            }
 
-        showRandomTask(taskFile)
+            override fun onFinished() {
+                binding.nextButton.text = getString(R.string.start_timer)
+                binding.timerText.text = ""
+                binding.primaryText.text = ""
+                showSecondaryTexts(emptyList())
+                @Suppress("DEPRECATION")
+                vibrator.vibrate(200)
+            }
+        }
+
         binding.nextButton.setOnClickListener {
+            if (timer.getState() == StatefulTimer.States.STOPPED) {
+                binding.nextButton.text = getString(R.string.next_button)
+                timer.start()
+            }
             showRandomTask(taskFile)
         }
     }
 
-    private fun getTimer(vibrator: Vibrator): CountDownTimer {
-        return object : CountDownTimer(60000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                binding.timerText.text = (millisUntilFinished / 1000).toString()
+    abstract class StatefulTimer {
+        enum class States { STOPPED, RUNNING }
+
+        private var state = States.STOPPED
+        abstract fun onTick(secondsUntilFinished: Int)
+        abstract fun onFinished()
+        private val timer = object : CountDownTimer(60000, 1000) {
+            override fun onTick(p0: Long) {
+                onTick((p0 / 1000).toInt())
             }
 
             override fun onFinish() {
-                binding.timerText.text = 0.toString()
-                @Suppress("DEPRECATION")
-                vibrator.vibrate(200)
+                Log.i(TAG, "StatefulTimer finished")
+                onFinished()
+                state = States.STOPPED
             }
+        }
+
+        fun getState(): States {
+            return state
+        }
+
+        fun start() {
+            timer.start()
+            state = States.RUNNING
+            Log.i(TAG, "StatefulTimer started")
         }
     }
 
@@ -51,9 +85,11 @@ class MainActivity : AppCompatActivity() {
         if (chosenTask == null) {
             binding.primaryText.text = ""
             showSecondaryTexts(emptyList())
+            Log.w(TAG, "failed to show next random task")
         } else {
-            binding.primaryText.text = chosenTask.guessWord.uppercase()
+            binding.primaryText.text = chosenTask.guessWord
             showSecondaryTexts(chosenTask.blockedWords.shuffled().subList(0, 5))
+            Log.i(TAG, "show random word '${chosenTask.guessWord}'")
         }
     }
 
@@ -80,6 +116,7 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class GuessTask(val guessWord: String, blockedWords: List<String>) {
+class GuessTask(guessWord: String, blockedWords: List<String>) {
+    val guessWord = guessWord.uppercase()
     val blockedWords = blockedWords.map { a -> a.uppercase() }
 }
