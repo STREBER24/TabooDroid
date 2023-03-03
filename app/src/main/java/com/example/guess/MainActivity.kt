d6aeae7fdf40e2b4505a4b6934ad96da3e9e5afc
Lars G <largot@gmx.de>
2023-03-03 19:03:52 +0100

    create settings activity

--- app/src/main/java/com/example/guess/MainActivity.kt
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.guess
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Vibrator
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
 import com.example.guess.databinding.ActivityMainBinding
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var timer: StatefulTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +31,11 @@ class MainActivity : AppCompatActivity() {
             Toast.LENGTH_LONG
         ).show()
 
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+
         @Suppress("DEPRECATION")
         val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
-        val timer = object : StatefulTimer(60) {
+        timer = object : StatefulTimer(preferences.getInt("timer_duration", 60)) {
             override fun onTick(secondsUntilFinished: Int) {
                 binding.timerText.text = secondsUntilFinished.toString()
                 if (secondsUntilFinished < 4) {
@@ -52,6 +59,23 @@ class MainActivity : AppCompatActivity() {
             }
             showRandomTask(tasks)
         }
+    }
+
+    override fun onDestroy() {
+        timer.cancel()
+        super.onDestroy()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.settings_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> startActivity(Intent(this, SettingsActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun showRandomTask(tasks: List<GuessTask>) {
